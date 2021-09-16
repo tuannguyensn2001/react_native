0b6f8c663c38fcd2321c853427dd7ca6723eb805
tuannguyensn2001 <devpro2001@gmail.com>
2021-09-16 09:04:32 +0700

    init

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { useEffect } from 'react';
+import { Button, ThemeProvider } from 'react-native-elements';
 
 export default function App() {
+
+  useEffect(() => {
+    console.log("hello");
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <Text>Hello</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+
+    <ThemeProvider>
+      <Button title="hello" loading={true}></Button>
+      <Text>Hello</Text>
+    </ThemeProvider>
   );
 }
 
